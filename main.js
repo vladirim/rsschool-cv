c0b775d7eace95b41e9c32b3dbfab9a1706c8572
vladirim <vladimir0lomako@gmail.com>
2022-09-24 11:23:59 -0400

    feat: add dark/light theme

--- main.js
+++ main.js
@@ -34,3 +34,36 @@ function scrollTop() {
 }
 
 window.addEventListener('scroll', scrollTop)
+
+
+/*===== DARK/LIGHT THEME =====*/
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+//Previously selected theme (if user selected)
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// Obtain current theme by validating dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+// Validate if theme is chosen
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+} 
+
+// Activation / deactivation of theme with button
+
+themeButton.addEventListener('click', () => {
+    // Add / remove dark icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // Saving of current theme
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
